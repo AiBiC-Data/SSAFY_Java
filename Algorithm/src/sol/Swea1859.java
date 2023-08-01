a779d8d54d8084746335dd5158089a86a0786daa
AiBiC-Data <whrnjsgh12@naver.com>
2023-08-01 23:56:48 +0900

    solving swea 1859

---
+++ Algorithm/src/sol/Swea1859.java
@@ -0,0 +1,19 @@
+package sol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Swea1859 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc < T+1; tc++) {
+			
+		}
+	}
+
+}
